9d1f056fefb358a724e81e86537fdbee10885d19
Zack Angelo <zackangelo@gmail.com>
2019-06-18 18:42:49 -0700

    fix things

--- src/convert.rs
+++ src/convert.rs
@@ -21,6 +21,54 @@ fn write_record_data<'a, W: Write, I: Iterator<Item = &'a RData>>(
                 write!(out, "\t\t\"{}\",\n", name)?;
                 Ok::<(), io::Error>(())
             }
+            RData::TXT(txt) => { 
+                let txt_data = txt.txt_data(); 
+
+                write!(out, "\t\t\"")?;
+
+                for txt_block in txt_data {
+                    let mut txt = Vec::new(); 
+                    let mut i: u32 = 0;
+                    
+                    txt.push('\\');
+                    txt.push('"');
+
+                    for c in txt_block.into_iter() { 
+
+                        // From TF docs: 
+                        // To specify a single record value longer than 255 characters such as a 
+                        // TXT record for DKIM, add \"\" inside the Terraform configuration 
+                        // string (e.g. "first255characters\"\"morecharacters").
+                        // 
+                        // https://www.terraform.io/docs/providers/google/r/dns_record_set.html#rrdatas
+                        if i > 0 && i % 255 == 0 { 
+                            txt.push('\\');
+                            txt.push('"');
+                            txt.push('\\');
+                            txt.push('"');
+                        }
+
+                        match c { 
+                            &c if c == b'"' => { 
+                                txt.push('\\');
+                                txt.push('"');
+                            }
+                            &c => txt.push(c as char),
+                        }
+
+                        i += 1;
+                    }
+
+                    txt.push('\\');
+                    txt.push('"');
+
+                    write!(out,"{}",txt.into_iter().collect::<String>())?; 
+                }
+
+                write!(out, "\",\n")?;
+
+                Ok::<(), io::Error>(())
+            }
             _ => Ok::<(), io::Error>(()),
         })?;
     }
@@ -28,6 +76,17 @@ fn write_record_data<'a, W: Write, I: Iterator<Item = &'a RData>>(
     write!(out, "\t]\n")?;
 
     Ok(())
+}
+
+fn record_resource_name(record_name: &Name, record_type: &RecordType) -> String { 
+    let mut record_name_str = record_name.to_utf8(); 
+    record_name_str = record_name_str.replace(".", "_"); 
+
+    if record_name_str.ends_with("_") { 
+        record_name_str.pop();
+    } 
+    
+    format!("{}_{}", record_name_str, record_type)
 }
 
 fn write_record_info<'a, W: Write,I: Iterator<Item = &'a Record>>(
@@ -38,11 +97,12 @@ fn write_record_info<'a, W: Write,I: Iterator<Item = &'a Record>>(
     rs: I, 
     out: &mut W) -> io::Result<()> {
 
+    let record_resource_name = record_resource_name(record_name, record_type);
     let type_str: &str = (*record_type).into(); 
 
-    write!(out, "resource \"google_dns_record_set\" \"{}\" {{\n", type_str.to_lowercase())?; 
+    write!(out, "resource \"google_dns_record_set\" \"{}\" {{\n", record_resource_name)?; 
     write!(out, "\tname = \"{}\"\n", record_name)?;
-    write!(out, "\tmanaged_zone = \"${{{}}}\"\n", zone_resource)?;
+    write!(out, "\tmanaged_zone = \"${{google_dns_managed_zone.{}.name}}\"\n", zone_resource)?;
     write!(out, "\ttype = \"{}\"\n", type_str)?;
     write!(out, "\tttl = {}\n", record_ttl)?;
 
